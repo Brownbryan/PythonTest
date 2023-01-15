6bb05d3008c078a702d098712b120c6af4c55289
brownbryan <muriukibryan11@gmail.com>
2023-01-15 14:25:02 +0300

    d

---
+++ bandnamegenpractice.py
@@ -0,0 +1,4 @@
+print("Welcome to the band generator")
+city=  input("What's the name of the city you grew up in \n")
+pet=input("What is your pet's name \n")
+print("your band name could be \n" + city + " " + pet)
