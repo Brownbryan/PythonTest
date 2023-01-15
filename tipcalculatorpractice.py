a0e3bf577557ec5a5f42c56ab02c17f2e8f1c452
brownbryan <muriukibryan11@gmail.com>
2023-01-15 15:06:23 +0300

    e

--- tipcalculatorpractice.py
+++ tipcalculatorpractice.py
@@ -1,6 +1,7 @@
 two_digit_number=input("")
 
 # Check the data type of two_digit_number
+# print(type(two_digit_number))
 first_digit=int(two_digit_number[0])
 second_digit=int(two_digit_number[1])
 #add the two digits number
