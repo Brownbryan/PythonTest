da320eae23737fb0ba7f291a31d623995096962c
brownbryan <muriukibryan11@gmail.com>
2023-01-15 15:05:26 +0300

    g

--- tipcalculatorpractice.py
+++ tipcalculatorpractice.py
@@ -1,1 +1,8 @@
-number = "0797209133"
+two_digit_number=input("")
+
+# Check the data type of two_digit_number
+first_digit=int(two_digit_number[0])
+second_digit=int(two_digit_number[1])
+#add the two digits number
+two_digit_number=first_digit+second_digit
+print(two_digit_number)
